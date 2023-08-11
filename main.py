29e01500b6f4bd380494940dfc3cfafaa1e8b372
GGTop22 <tikhomgonzo2203@gmail.com>
2023-08-11 16:25:49 +0200

    добавил видемые ходы фигур ,обозначающиеся кружками

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from PIL import ImageTk, Image
+from functools import partial
 
 
 def symbolsToIntCoord(k: str) -> (int, int):
@@ -24,12 +26,51 @@ black = {}
 white = {}
 
 
+def isKnightJump(p1: (int, int), p2: (int, int)) -> bool:
+    return (abs(p1[0] - p2[0]) == 2 and abs(p1[1] - p2[1]) == 1) or (
+            abs(p1[0] - p2[0]) == 1 and abs(p1[1] - p2[1]) == 2)
+
+
+def isBishopJump(p1: (int, int), p2: (int, int)) -> bool:
+    return (abs(p1[0] - p2[0])) == (abs(p1[1] - p2[1]))
+
+
+def isQueenJump(p1: (int, int), p2: (int, int)) -> bool:
+    return isBishopJump(p1, p2) or isRookJump(p1, p2)
+
+
+def isRookJump(p1: (int, int), p2: (int, int)) -> bool:
+    return (p1[0] == p2[0]) or (p1[1] == p2[1])
+
+
+def isKingJump(p1: (int, int), p2: (int, int)) -> bool:
+    return (abs(p1[0] - p2[0])) <= 1 and (abs(p1[1] - p2[1])) <= 1
+
+
+def isPawnJump(p1: (int, int), p2: (int, int), colour) -> bool:  # Не больше 1 /бить по диоганали
+    # если пешка белая то ,если она на второй линии то она может идти на 2 поля вперёд,иначе на 1 поле вперёд
+    if colour == "red":
+        if p1[1] == 2:  # истина это если не изменился столбец ,а строка увеличилась либо на 1 или на 2
+            return (p1[0] == p2[0]) and (p2[1] - p1[1]) in (1, 2)
+        else:  # #истина это если не изменился столбец ,а строка увеличилась на 1
+            return (p1[0] == p2[0]) and (p2[1] - p1[1]) == 1
+    else:
+        if p1[
+            1] == 7:  # если пешка чёрная то ,если она на 7 линии то она может идти на 2 поля вперёд,иначе на 1 поле вперёд
+            return (p1[0] == p2[0]) and (p2[1] - p1[1]) in (
+            -1, -2)  # истина это если не изменился столбец ,а строка уменьшилась либо на -1 или на -2
+        else:
+            return (p1[0] == p2[0]) and (
+                        p2[1] - p1[1]) == -1  # истина это если не изменился столбец ,а строка  уменьшилась на -1
+
+
 def drawDesk(white, black):
     recSize = 60
     x1 = 230
     y1 = 600
     x2 = x1 + recSize
     y2 = y1 + recSize
+    clearPossibleMoves()
     for g in range(1, 9):
         # rec = canvas.create_rectangle(x1, y1 - recSize * g, x2, y2 - recSize * g)
         for i in range(1, 9):
@@ -43,16 +84,87 @@ def drawFigure(figurePoint, white, black, recSize, x1, y1):
     g = figurePoint[1]
     if figurePoint in black:
         figureName = black[figurePoint]
-        drawLabel(figureName, 'black', x1 + recSize * i + 22, y1 - recSize * g + 20)
+        drawLabel(figureName, 'black', x1 + recSize * i + 22, y1 - recSize * g + 20, figurePoint)
     if figurePoint in white:
         figureName = white[figurePoint]
-        drawLabel(figureName, "red", x1 + recSize * i + 22, y1 - recSize * g + 20)
+        drawLabel(figureName, "red", x1 + recSize * i + 22, y1 - recSize * g + 20, figurePoint)
 
 
-def drawLabel(text, colour, x, y):
-    label = Label(canvas, text=text)
-    label.configure(font="Helvetica 15 bold", fg=colour)
-    label.place(x=x, y=y)
+def drawLabel(text, colour, x, y, figPoint):
+    if text == "кр":
+        file = "King"
+    if text == "ф":
+        file = "Queen"
+    if text == "с":
+        file = "Bishop"
+    if text == "к":
+        file = "Knight"
+    if text == "л":
+        file = "Rook"
+    if text == "п":
+        file = "Pawn"
+    file = "img/" + colour.title() + file + ".jpg"
+
+    img = ImageTk.PhotoImage(Image.open(file).resize((55, 55), Image.BICUBIC))
+    btn = Button(canvas, image=img, command=partial(click, text, figPoint, colour))
+    btn.photo = img
+    btn.config(height=55, width=55)
+    # label.configure(font="Helvetica 15 bold", fg=colour)
+    btn.place(x=x - 21, y=y - 19)
+
+
+# def drawMoveDots():
+# dots=canvas.create_oval(coords,outline="greena",fill="green",width=3)
+figure = None  # Глобальная переменная для хранения выбранной фигуры
+
+
+def click(text, figPoint, colour):
+    global figure
+    figure = (text, figPoint, colour)
+    print("click click" + "  " + colour + "  " + text + "  " + str(figPoint))
+    spisok = []
+    for i1 in range(1, 9):
+        for i2 in range(1, 9):
+            p = (i1, i2)
+            if text == "ф" and isQueenJump(figPoint, p) and figPoint != p:
+                spisok.append(p)
+            if text == "л" and isRookJump(figPoint, p) and figPoint != p:
+                spisok.append(p)
+            if text == "к" and isKnightJump(figPoint, p) and figPoint != p:
+                spisok.append(p)
+            if text == "с" and isBishopJump(figPoint, p) and figPoint != p:
+                spisok.append(p)
+            if text == "кр" and isKingJump(figPoint, p) and figPoint != p:
+                spisok.append(p)
+            if text == "п" and isPawnJump(figPoint, p, colour) and figPoint != p:
+                spisok.append(p)
+    clearPossibleMoves()
+    drawPossibleMoves(spisok)
+    print(spisok)
+
+
+def drawPossibleMoves(possible_moves):
+    for move in possible_moves:
+        i, g = move
+        x = 230 + 60 * i
+        y = 600 - 60 * g
+        canvas.create_oval(x, y, x + 60, y + 60, outline="green", width=3, tags="green_oval")
+
+
+def clearPossibleMoves():
+    canvas.delete("green_oval")
+
+
+# im = PhotoImage(file='myfile.gif')
+# l = Label(root, image=im)
+
+# btn = _.Button(file='myfile.gif')
+# l = Label(root, image=im)
+
+# def drawLabel(file, colour, x, y):
+# l = Label(canvas, file='BlackQueen.jpg', fg=colour)
+#  label = Label(root, image=l)
+# label.place(x=x, y=y)
 
 
 with open('INPUT.TXT', 'r', encoding="UTF-8") as input:
